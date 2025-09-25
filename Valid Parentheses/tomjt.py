14c68db4b43865f71d808273cf59db4d823fd719
tom tait <thomas.t.ca@outlook.com>
2025-09-25 17:12:08 -0300

    got rid of debug vars

--- Valid Parentheses/tomjt.py
+++ Valid Parentheses/tomjt.py
@@ -1,8 +1,5 @@
 class Solution(object):
     def isValid(self, s):
-        p = 0
-        b = 0
-        c = 0
         ans = []
         for i in s:
           if ((i == '(') or (i == '[') or (i == '{')):

a8fd0b43b27c1443433533d5ff7a8cefaf4e2cbd
dokngu <dokngu@gmail.com>
2025-10-16 16:02:58 -0300

    feat: added non-recursive code for a LL problem

---
+++ Merge Two Sorted Linked Lists/dokngu_iterative.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // keep reference to return later
+        ListNode prehead(-1);
+        ListNode* prev = &prehead;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+
+        l1? prev->next = l1 : prev->next = l2;
+        
+        //return head
+        return prehead.next;
+    }
+};
